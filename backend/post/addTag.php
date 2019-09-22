d8e066fa86959751eb753692ba1ec8c2c6e124c5
zavrik94 <arty.zavrik@gmail.com>
2019-09-22 16:20:19 +0300

    some changes

---
+++ backend/post/addTag.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace backend\post;
+
+use backend\Error;
+
+class addTag
+{
+    private $conn;
+    private $post;
+    private $cur_id;
+
+    function __construct() {
+        $postData = file_get_contents('php://input');
+        $this->post = json_decode($postData, true);
+        $this->conn = new \PDO('mysql:host=' . DBHOST . ':' . DBPOST . ';dbname=' . DBNAME, DBLOGIN, DBPASS);
+    }
+
+     public function add() {
+        if (!$this->post['name']) {
+            $error = new Error(9);
+            return $error->getError();
+        }
+        $date = date('Y-m-d');
+        $name = $this->post['name'];
+        $this->conn->query("INSERT INTO tag SET name='$name',date='$date'");
+        $this->cur_id=$this->conn->lastInsertId();
+        $result[] = ["error" => false];
+        $result[] = ["id" => $this->cur_id];
+        $this->addRelations($this->post['tasks']);
+        return json_encode($result);
+    }
+
+    private function addRelations($arr) {
+        if ($arr == null)
+            return ;
+        foreach ($arr as $cur_item) {
+            $this->conn->query("INSERT INTO relations SET id_task=$cur_item,id_tag=$this->cur_id");
+        }
+    }
+}
---
+++ backend/post/addTask.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace backend\post;
+
+use backend\Error;
+
+class addTask
+{
+    private $conn;
+    private $post;
+    private $cur_id;
+
+    function __construct() {
+        $postData = file_get_contents('php://input');
+        $this->post = json_decode($postData, true);
+        $this->conn = new \PDO('mysql:host=' . DBHOST . ':' . DBPOST . ';dbname=' . DBNAME, DBLOGIN, DBPASS);
+    }
+
+    public function add() {
+        if (!$this->post['name']) {
+            $error = new Error(7);
+            return $error->getError();
+        }
+        elseif (!$this->post['description']) {
+            $error = new Error(8);
+            return $error->getError();
+        }
+        $date = date('Y-m-d');
+        $name = $this->post['name'];
+        $desc = $this->post['description'];
+        $this->conn->query("INSERT INTO task SET name='$name',description='$desc',date='$date'");
+        $this->cur_id=$this->conn->lastInsertId();
+        $result = ["error" => false];
+        $result["id"] = (int)$this->cur_id;
+        $this->addRelations($this->post['tags']);
+        return json_encode($result);
+    }
+
+    private function addRelations($arr) {
+        if ($arr == null)
+            return ;
+        foreach ($arr as $cur_item) {
+            $this->conn->query("INSERT INTO relations SET id_task=$this->cur_id,id_tag=$cur_item");
+        }
+    }
+}
